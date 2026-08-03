d37a85e70682a9ab4215540eeb4304c4096ccdec
venu <venuvatsavai66@gmail.com>
2026-08-03 15:25:39 +0530

    Add files via upload

--- getPlatform.js
+++ getPlatform.js
@@ -16,28 +16,23 @@ var __copyProps = (to, from, except, desc) => {
   return to;
 };
 var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
-var dist_exports = {};
-__export(dist_exports, {
-  assertNodeAPISupported: () => import_chunk_O5EOXX3N.assertNodeAPISupported,
-  binaryTargets: () => import_chunk_7MLUNQIZ.binaryTargets,
+var getPlatform_exports = {};
+__export(getPlatform_exports, {
+  computeLibSSLSpecificPaths: () => import_chunk_YDM7ULQH.computeLibSSLSpecificPaths,
+  getArchFromUname: () => import_chunk_YDM7ULQH.getArchFromUname,
   getBinaryTargetForCurrentPlatform: () => import_chunk_YDM7ULQH.getBinaryTargetForCurrentPlatform,
-  getNodeAPIName: () => import_chunk_2U36ISZO.getNodeAPIName,
+  getBinaryTargetForCurrentPlatformInternal: () => import_chunk_YDM7ULQH.getBinaryTargetForCurrentPlatformInternal,
   getPlatformInfo: () => import_chunk_YDM7ULQH.getPlatformInfo,
+  getPlatformInfoMemoized: () => import_chunk_YDM7ULQH.getPlatformInfoMemoized,
+  getSSLVersion: () => import_chunk_YDM7ULQH.getSSLVersion,
   getos: () => import_chunk_YDM7ULQH.getos,
-  jestConsoleContext: () => import_chunk_M5NKJZ76.jestConsoleContext,
-  jestContext: () => import_chunk_M5NKJZ76.jestContext,
-  jestProcessContext: () => import_chunk_M5NKJZ76.jestProcessContext,
-  link: () => import_chunk_D7S5FGQN.link
+  parseDistro: () => import_chunk_YDM7ULQH.parseDistro,
+  parseLibSSLVersion: () => import_chunk_YDM7ULQH.parseLibSSLVersion,
+  parseOpenSSLVersion: () => import_chunk_YDM7ULQH.parseOpenSSLVersion,
+  resolveDistro: () => import_chunk_YDM7ULQH.resolveDistro
 });
-module.exports = __toCommonJS(dist_exports);
-var import_chunk_6HZWON4S = require("./chunk-6HZWON4S.js");
-var import_chunk_M5NKJZ76 = require("./chunk-M5NKJZ76.js");
-var import_chunk_O5EOXX3N = require("./chunk-O5EOXX3N.js");
-var import_chunk_2U36ISZO = require("./chunk-2U36ISZO.js");
+module.exports = __toCommonJS(getPlatform_exports);
 var import_chunk_YDM7ULQH = require("./chunk-YDM7ULQH.js");
-var import_chunk_D7S5FGQN = require("./chunk-D7S5FGQN.js");
 var import_chunk_FWMN4WME = require("./chunk-FWMN4WME.js");
 var import_chunk_YVXCXD3A = require("./chunk-YVXCXD3A.js");
-var import_chunk_7MLUNQIZ = require("./chunk-7MLUNQIZ.js");
 var import_chunk_2ESYSVXG = require("./chunk-2ESYSVXG.js");
-(0, import_chunk_7MLUNQIZ.init_binaryTargets)();
--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,7 @@
-export { type BaseContext, jestConsoleContext, jestContext, jestProcessContext } from './jestContext';
+export { assertNodeAPISupported } from './assertNodeAPISupported';
+export { type BinaryTarget, binaryTargets } from './binaryTargets';
+export { getNodeAPIName } from './getNodeAPIName';
+export type { PlatformInfo } from './getPlatform';
+export { getBinaryTargetForCurrentPlatform, getos, getPlatformInfo } from './getPlatform';
+export { link } from './link';
+export * from './test-utils';
