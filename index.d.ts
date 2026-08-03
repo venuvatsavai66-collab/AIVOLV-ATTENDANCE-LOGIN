b4f0f7ce8512f069f5bb9e3fcfd302ea5ff3b96c
venu <venuvatsavai66@gmail.com>
2026-08-03 16:49:57 +0530

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,44 +1,28 @@
-declare function arg<T extends arg.Spec>(
-	spec: T,
-	options?: arg.Options
-): arg.Result<T>;
-
-declare namespace arg {
-	export const flagSymbol: unique symbol;
-
-	export function flag<T>(fn: T): T & { [arg.flagSymbol]: true };
-
-	export const COUNT: Handler<number> & { [arg.flagSymbol]: true };
-
-	export type Handler<T = any> = (
-		value: string,
-		name: string,
-		previousValue?: T
-	) => T;
-
-	export class ArgError extends Error {
-		constructor(message: string, code: string);
-
-		code: string;
-	}
-
-	export interface Spec {
-		[key: string]: string | Handler | [Handler];
-	}
-
-	export type Result<T extends Spec> = { _: string[] } & {
-		[K in keyof T]?: T[K] extends Handler
-			? ReturnType<T[K]>
-			: T[K] extends [Handler]
-			? Array<ReturnType<T[K][0]>>
-			: never;
-	};
-
-	export interface Options {
-		argv?: string[];
-		permissive?: boolean;
-		stopAtPositional?: boolean;
-	}
-}
-
-export = arg;
+import { BrowserVersion, Options, AllVersionsOptions, AllBrowsersBrowserVersion, NestedBrowserVersions, TimelineOptions, TimelineEvent, BrowserTimeline } from "./types.js";
+export declare function _resetHasWarned(): void;
+/**
+ * Returns browser versions compatible with specified Baseline targets.
+ * Defaults to returning the minimum versions of the core browser set that support Baseline Widely available.
+ * Takes an optional configuration `Object` with four optional properties:
+ * - `listAllCompatibleVersions`: `false` (default) or `true`
+ * - `includeDownstreamBrowsers`: `false` (default) or `true`
+ * - `widelyAvailableOnDate`: date in format `YYYY-MM-DD`
+ * - `targetYear`: year in format `YYYY`
+ * - `supressWarnings`: `false` (default) or `true`
+ */
+export declare function getCompatibleVersions(userOptions?: Options): BrowserVersion[];
+/**
+ * Returns all browser versions known to this module with their level of Baseline support as a JavaScript `Array` (`"array"`), `Object` (`"object"`) or a CSV string (`"csv"`).
+ * Takes an optional configuration `Object` with three optional properties:
+ * - `includeDownstreamBrowsers`: `false` (default) or `true`
+ * - `outputFormat`: `"array"` (default), `"object"` or `"csv"`
+ * - `useSupports`: `false` (default) or `true`, replaces `wa_compatible` property with optional `supports` property which returns `widely` or `newly` available when present.
+ * - `supressWarnings`: `false` (default) or `true`
+ */
+export declare function getAllVersions(userOptions?: AllVersionsOptions): AllBrowsersBrowserVersion[] | NestedBrowserVersions | string;
+export declare function getTimeline(userOptions?: TimelineOptions & {
+    groupBy?: "date";
+}): TimelineEvent[];
+export declare function getTimeline(userOptions: TimelineOptions & {
+    groupBy: "browser";
+}): BrowserTimeline;
--- utils.d.ts
+++ utils.d.ts
@@ -1,11 +1,1 @@
-import { BinaryTarget } from '@prisma/get-platform';
-export declare function getRootCacheDir(): Promise<string | null>;
-export declare function getCacheDir(channel: string, version: string, binaryTarget: string): Promise<string | null>;
-export declare function getDownloadUrl({ channel, version, binaryTarget, binaryName, extension, }: {
-    channel: string;
-    version: string;
-    binaryTarget: BinaryTarget;
-    binaryName: string;
-    extension?: string;
-}): string;
-export declare function overwriteFile(sourcePath: string, targetPath: string): Promise<void>;
+export declare const compareVersions: (nextVersion: string, prevVersion: string) => 1 | 0 | -1;
